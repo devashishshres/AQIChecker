fb535d64d4ca45a81c320fce80630add00333268
Devashish Shrestha <shresthdevashish@gmail.com>
2025-07-04 20:28:53 +0545

    Minor change

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -9,7 +9,6 @@ interface SearchProps {
 export const Search = ({ city, setCity, handleSubmit }: SearchProps) => {
 
   return (
-
     <form id="locationSearchForm" method="post" className="flex flex-row bg-[#141635] rounded-lg px-4 py-4 items-center justify-between" onSubmit={handleSubmit}>
         <input type="text" placeholder="Enter your city" autoComplete="off" className="text-2xl bg-transparent border-none outline-none" value={city} onChange={e => setCity(e.target.value)}/>
         <button className="cursor-pointer" type="submit"><FaSearchLocation className="w-6 h-6 text-[#737373] hover:text-[#cccccc]"/></button> 
