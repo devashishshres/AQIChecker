4a0e6ed3d90a00b215ba0c9878c0e7c731278636
Devashish Shrestha <shresthdevashish@gmail.com>
2025-07-04 16:51:41 +0545

    Implement logic to handle search form submission, call backend api, manage loading and error states, and display aqi result

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,58 @@
+import { useState } from 'react'
 import './App.css'
 import { Search } from './components/Search'
+import type { AQIResponse, aqiData } from './AQI';
+
 
 function App() {
+  const [city, setCity] = useState("");
+  const [apiData, setApiData] = useState<aqiData | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  
+  const fetchAqiData = async(city: string): Promise<AQIResponse> => {
+    const response = await fetch(`/api/search?keyword=${encodeURIComponent(city)}`)
+    
+    if (!response.ok) throw new Error("Failed to fetch!");
+    return response.json();
+  }
+
+  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+    console.log("Form submitted!")
+    e.preventDefault();
+    setLoading(true);
+    setError("");
+
+    try {
+      const data = await fetchAqiData(city);
+      setApiData(data.data);
+      console.log("👀 apiData:", data.data);
+    } catch (error) {
+      setError("Could not fetch API data! Error: " + (error as Error).message);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <main>
       <div className='flex flex-col items-center min-h-screen gap-14'>
         <h1 className='mt-12 text-5xl font-semibold'>AirAware</h1>
-        <Search />
+        <Search city={city} setCity={setCity} handleSubmit={handleSubmit}/>
+
+        {loading && <div className='text-lg'>Loading</div>}
+
+        {error && <div className='text-lg bg-red-600'>Error</div>}
+
+        {apiData && (
+          <div>
+            AQI: {apiData.aqi}
+          </div>
+        )}
       </div>
     </main>
   )
 }
 
 export default App
+
